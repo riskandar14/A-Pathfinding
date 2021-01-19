e5dc07d25f5fc6bb24eedd16a1147b31f5e85539
Ryan Iskandar <44584843+riskandar14@users.noreply.github.com>
2021-01-18 20:55:56 -0800

    Upload

---
+++ Assets/Pathfinding/Scripts/Grid.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Grid<TGridObject> {
+
+    //event declaration and triggers updates in values/objects
+    public event EventHandler<OnGridObjectChangedEventArgs> OnGridObjectChanged;
+    public class OnGridObjectChangedEventArgs : EventArgs {
+        public int x;
+        public int y;
+    }
+
+    //variables used to create grid
+    private int width;
+    private int height;
+    private float cellSize;
+    private Vector3 originPosition;
+    private TGridObject[,] gridArray; //multidimensional array with two dimensions
+
+    //instantiation of grid
+    public Grid(int width, int height, float cellSize, Vector3 originPosition, Func<Grid<TGridObject>, int, int, TGridObject> createGridObject) {
+        //assigning parameters to variables
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+
+        //create grid with desired width and height
+        gridArray = new TGridObject[width, height];
+
+        //Loop through all grid spaces
+        for (int x = 0; x < gridArray.GetLength(0); x++) {
+            for (int y = 0; y < gridArray.GetLength(1); y++) {
+                gridArray[x, y] = createGridObject(this, x, y);
+            }
+        }
+
+        //displays x, y locations on grid if bool is set to true
+        bool showDebug = false;
+        if (showDebug) {
+            TextMesh[,] debugTextArray = new TextMesh[width, height];
+
+            for (int x = 0; x < gridArray.GetLength(0); x++) {
+                for (int y = 0; y < gridArray.GetLength(1); y++) {
+                    debugTextArray[x, y] = UtilsClass.CreateWorldText(gridArray[x, y]?.ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f, 30, Color.white, TextAnchor.MiddleCenter);
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+                }
+            }
+            //Draw the lines to show the grid map
+            Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+            Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+
+            OnGridObjectChanged += (object sender, OnGridObjectChangedEventArgs eventArgs) => {
+                debugTextArray[eventArgs.x, eventArgs.y].text = gridArray[eventArgs.x, eventArgs.y]?.ToString();
+            };
+        }
+    }
+
+    //Function to get width
+    public int GetWidth() {
+        return width;
+    }
+
+    //Function to get height
+    public int GetHeight() {
+        return height;
+    }
+
+    //Function to get cell size
+    public float GetCellSize() {
+        return cellSize;
+    }
+
+    //Function to change x, y coordinates to world position
+    public Vector3 GetWorldPosition(int x, int y) {
+        return new Vector3(x, y) * cellSize + originPosition;
+    }
+
+    //Function to change world position to x, y coordinates on grid
+    public void GetXY(Vector3 worldPosition, out int x, out int y) {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
+
+    //Sets x, y values within grid spaces
+    public void SetGridObject(int x, int y, TGridObject value) {
+        if (x >= 0 && y >= 0 && x < width && y < height) {
+            gridArray[x, y] = value;
+            if (OnGridObjectChanged != null) OnGridObjectChanged(this, new OnGridObjectChangedEventArgs { x = x, y = y });
+        }
+    }
+
+    //Triggers an event that allows for values to be changed
+    public void TriggerGridObjectChanged(int x, int y) {
+        if (OnGridObjectChanged != null) OnGridObjectChanged(this, new OnGridObjectChangedEventArgs { x = x, y = y });
+    }
+
+    //Sets Vector3 value within grid spaces
+    public void SetGridObject(Vector3 worldPosition, TGridObject value) {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetGridObject(x, y, value);
+    }
+
+    //Retrieve x, y coordinates
+    public TGridObject GetGridObject(int x, int y) {
+        if (x >= 0 && y >= 0 && x < width && y < height) {
+            return gridArray[x, y];
+        } else {
+            return default(TGridObject);
+        }
+    }
+
+    //Retrieve the Vector3 world position
+    public TGridObject GetGridObject(Vector3 worldPosition) {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetGridObject(x, y);
+    }
+
+}
---
+++ Assets/Pathfinding/Scripts/PathNode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode {
+
+    //variables used to track spaces on the grid
+    private Grid<PathNode> grid;
+    public int x;
+    public int y;
+
+    //variables used to track the cost of moving spaces
+    public int gCost;
+    public int hCost;
+    public int fCost;
+
+    public bool isWalkable; //is the space open or blocked off
+    public PathNode cameFromNode; //previous node that we used, helpful for tracing backwards
+
+    //Creation of a node that follows the grid's locations
+    public PathNode(Grid<PathNode> grid, int x, int y) {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+        isWalkable = true;
+    }
+
+    //Function to calculate the F cost
+    public void CalculateFCost() {
+        fCost = gCost + hCost;
+    }
+
+    public void SetIsWalkable(bool isWalkable) {
+        this.isWalkable = isWalkable;
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public override string ToString() {
+        return x + "," + y;
+    }
+
+}
